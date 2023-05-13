f9be14b42aed776d0c30410a8e3e45144165ddde
Aaron Trelstad <trelstadaaron@gmail.com>
2023-05-13 10:33:11 -0500

    article scraping

--- static.py
+++ static.py
@@ -22,12 +22,12 @@ for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
+    link_url = job_element.find_all("a")[1]["href"]
     print(title_element.text.strip())
     print(company_element.text.strip())
     print(location_element.text.strip())
+    print(link_url)
     print()
 
-for job_element in python_job_elements:
-    link_url = job_element.find_all("a")[1]["href"]
-    print(f"Apply here: {link_url}\n")
+
 
